be20cf0ef7d0ffc3b654e5fd8a853711e88eb3d6
songminwoo1 <mwsong@webeng.kaist.ac.kr>
2025-05-02 08:11:30 +0900

    Update net_service.go

--- net_service/net_service.go
+++ net_service/net_service.go
@@ -126,9 +126,9 @@ func NewDefaultTlsConf(tls_identity *TLSIdentity) *tls.Config {
 
 func NewDefaultQuicConf() *quic.Config {
 	return &quic.Config{
-		MaxIdleTimeout:                time.Minute,
+		MaxIdleTimeout:                time.Hour,
 		AllowConnectionWindowIncrease: func(conn quic.Connection, delta uint64) bool { return true },
-		KeepAlivePeriod:               time.Second * 10,
+		KeepAlivePeriod:               time.Second * 5,
 		Allow0RTT:                     true,
 		EnableDatagrams:               true,
 	}
